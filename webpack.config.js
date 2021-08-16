7be994283868182eb1996a6d85c54cd0f1f02e81
Hoticus <65857100+Hoticus@users.noreply.github.com>
2021-08-17 00:05:57 +0300

    Update symfony to 5.3.6 and update npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,9 @@ if (Encore.isProduction()) {
 
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath("public/build/")
+    .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath("/build")
+    .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
@@ -33,11 +33,11 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry("app", "./assets/app.js")
-    .addEntry("accountSettings", "./assets/accountSettings.js")
+    .addEntry('app', './assets/app.js')
+    .addEntry('accountSettings', './assets/accountSettings.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge("./assets/controllers.json")
+    .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -73,32 +73,33 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     .configureBabel((config) => {
-        config.plugins.push("@babel/plugin-proposal-class-properties");
+        config.plugins.push('@babel/plugin-proposal-class-properties');
     })
 
     // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = "usage";
+        config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
 
     // enables PostCSS support
     .enablePostCssLoader();
 
-// enables Sass/SCSS support
-//.enableSassLoader()
+    // enables Sass/SCSS support
+    //.enableSassLoader()
 
-// uncomment if you use TypeScript
-//.enableTypeScriptLoader()
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
 
-// uncomment if you use React
-//.enableReactPreset()
+    // uncomment if you use React
+    //.enableReactPreset()
 
-// uncomment to get integrity="..." attributes on your script & link tags
-// requires WebpackEncoreBundle 1.4 or higher
-//.enableIntegrityHashes(Encore.isProduction())
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
 
-// uncomment if you're having problems with a jQuery plugin
-//.autoProvidejQuery()
+    // uncomment if you're having problems with a jQuery plugin
+    //.autoProvidejQuery()
+;
 
 module.exports = Encore.getWebpackConfig();
